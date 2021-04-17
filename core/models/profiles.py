ba434b87beeca7fa535102769a60ab3ac810301d
Defaultse <45491587+Defaultse@users.noreply.github.com>
2021-04-18 03:08:36 +0600

    manager to Product, Shop

--- core/models/profiles.py
+++ core/models/profiles.py
@@ -3,7 +3,6 @@ from utils.constants import *
 
 
 class ShopOwner(User):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     shop = models.OneToOneField("core.Shop", unique=True, on_delete=models.PROTECT)
 
     class Meta:
@@ -12,7 +11,6 @@ class ShopOwner(User):
 
 
 class Customer(User):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     location = models.CharField(choices=CITIES, max_length=30, blank=True)
     address = models.CharField(max_length=255, null=True, blank=True)
 
@@ -22,7 +20,6 @@ class Customer(User):
 
 
 class DeliveryGuy(User):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     shopId = models.ForeignKey("core.Shop", null=True, on_delete=models.PROTECT)
 
     class Meta:
--- core/models/shop.py
+++ core/models/shop.py
@@ -1,13 +1,19 @@
 from datetime import datetime
-
 from core.models import Customer
 from utils.constants import *
+
+
+class ShopManager(models.Manager):
+    def search(self, name):
+        return self.filter(name__contains=name)
 
 
 class Shop(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True, db_index=True)
     location = models.CharField(max_length=100, choices=CITIES, blank=True)
     description = models.TextField(max_length=255, null=True, blank=True)
+
+    objects = ShopManager()
 
     def __str__(self):
         return self.name
@@ -17,8 +23,14 @@ class Category(models.Model):
     name = models.CharField(max_length=180)
     shop = models.ForeignKey("core.Shop", on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class ProductManager(models.Manager):
+    def search(self, name):
+        return self.filter(name__contains=name)
+
     def get_by_category(self, category_id):
         return self.get_related().filter(category_id=category_id)
 
@@ -32,9 +44,14 @@ class Product(models.Model):
 
     objects = ProductManager()
 
+    def __str__(self):
+        return self.name
+
 
 class Subscription(models.Model):
     subscriber = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     subscribedProduct = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     startedAt = models.DateField(default=datetime.now())
     subscription_interval = models.SmallIntegerField(choices=SUBSCRIPTION_INTERVAL, default=7)
+
+
